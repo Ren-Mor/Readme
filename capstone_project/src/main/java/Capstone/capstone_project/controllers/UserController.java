d80067971a8df2f5fc000336e24b39b4b7a07c87
Ren-Mor <renatomorelli@hotmail.it>
2025-09-08 12:01:02 +0200

    fix endpoint

--- capstone_project/src/main/java/Capstone/capstone_project/controllers/UserController.java
+++ capstone_project/src/main/java/Capstone/capstone_project/controllers/UserController.java
@@ -3,12 +3,18 @@ package Capstone.capstone_project.controllers;
 
 import Capstone.capstone_project.entities.User;
 import Capstone.capstone_project.payloads.NewUserDTO;
+import Capstone.capstone_project.payloads.NewUserResponseDTO;
 import Capstone.capstone_project.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/utenti")
@@ -19,8 +25,12 @@ public class UserController {
 
     @GetMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public User getUserById(@RequestParam Long id) {
-        return this.utentiService.findById(id);
+    public List<NewUserResponseDTO> getAllUsers(@RequestParam (value = "page", defaultValue = "0")int page, @RequestParam(value = "size", defaultValue = "10")int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page <User> usersPage = this.utentiService.findAll(pageable);
+        return usersPage.stream().map(user -> new NewUserResponseDTO(
+                user.getId()
+        )).toList();
     }
 
     @DeleteMapping("/{id}")
--- capstone_project/src/main/java/Capstone/capstone_project/services/UserService.java
+++ capstone_project/src/main/java/Capstone/capstone_project/services/UserService.java
@@ -8,6 +8,7 @@ import Capstone.capstone_project.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -28,6 +29,10 @@ public class UserService {
     public User findById(Long userId) {
         return usersRepository.findById(userId)
                 .orElseThrow(() -> new BadRequestException("Utente non trovato"));
+    }
+
+    public Page <User> findAll(Pageable pageable){
+        return usersRepository.findAll(pageable);
     }
 
     public User findByIdAndUpdate(Long userId, NewUserDTO payload) {
